61d0b4da4573025227e0f2071a4c69f365e3c211
BlackSound1 <matthewr@videotron.ca>
2026-01-03 21:36:29 -0500

    Initial work

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AccentProvider } from "@/context/AccentContext";
 import { ColorCheckboxProvider } from "@/context/ColorCheckboxContext";
+import { Tile } from "@/components/doras-ui/tile";
+import StatusIndicator from "@/components/ui/status-indicator";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +36,25 @@ export default function RootLayout({
             {children}
           </ColorCheckboxProvider>
         </AccentProvider>
+        <div className="relative m-auto mx-5 mb-5">
+          <footer className="bg-crust text-subtext0 border-surface0/20 flex h-auto flex-col items-center justify-center gap-y-3 rounded-lg border p-5 text-sm md:flex-row md:justify-between md:gap-y-0">
+            <div className="flex flex-wrap items-center justify-center gap-x-3 gap-y-2 md:justify-start">
+              <div className="flex flex-wrap items-center justify-center gap-x-3 gap-y-2 md:justify-start">
+                <span className="whitespace-nowrap">&copy;2026 BlackSound1</span>
+                <span className="text-surface0 hidden md:inline">-</span>
+                <div className="">
+                  <StatusIndicator 
+                    state="active"
+                    label="All Systems Go!"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-center gap-x-3 gap-y-2 md:justify-end">
+              hey
+            </div>
+          </footer>
+        </div>
       </body>
     </html>
   );
