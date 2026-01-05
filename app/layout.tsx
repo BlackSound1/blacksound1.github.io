591c9bf8e3d3ea27f737af0901769e2934802e5c
BlackSound1 <matthewr@videotron.ca>
2026-01-05 15:15:11 -0500

    Refactor footer into own component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AccentProvider } from "@/context/AccentContext";
 import { ColorCheckboxProvider } from "@/context/ColorCheckboxContext";
-import StatusIndicator from "@/components/ui/status-indicator";
-import ViewCounter from "@/components/ui/ViewCounter";
-import CommitTracker from "@/components/ui/CommitTracker";
+import Footer from "@/components/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,22 +33,7 @@ export default function RootLayout({
         <AccentProvider>
           <ColorCheckboxProvider>
             {children}
-            <div className="relative m-auto mx-10 mb-10">
-              <footer className="bg-mantle hover:border-accent focus-visible:border-accent transition-colors text-muted-foreground font-semibold border-surface0/20 flex h-auto flex-col items-center justify-center gap-y-3 rounded-xl shadow-lg border p-5 text-sm md:flex-row md:justify-between md:gap-y-0">
-                <div className="flex flex-wrap items-center justify-center gap-x-3 gap-y-2 md:justify-start">
-                  <span className="whitespace-nowrap">&copy;2026 BlackSound1</span>
-                  <span className="text-surface0 hidden md:inline">-</span>
-                  <div className="">
-                    <StatusIndicator state="active" label="All Systems Go!" labelClassName="text-muted-foreground" />
-                  </div>
-                </div>
-                <div className="flex flex-wrap items-center justify-center gap-x-3 gap-y-2 md:justify-end">
-                  <ViewCounter />
-                  <span className="text-surface0 hidden md:inline">-</span>
-                  <CommitTracker />
-                </div>
-              </footer>
-            </div>
+            <Footer />
           </ColorCheckboxProvider>
         </AccentProvider>
       </body>
