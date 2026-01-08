5e93dd77ddc164d7b4c9b399864897dee884ecfd
BlackSound1 <matthewr@videotron.ca>
2026-01-07 21:12:48 -0500

    Prettier'd ./lib

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 /**
@@ -12,30 +12,30 @@ export function cn(...inputs: ClassValue[]) {
  * @returns The dimmed color as a hex string.
  */
 export function darkenHex(hex: string, percent: number): string {
-    let hexNumber = hex.replace('#', '');
+  let hexNumber = hex.replace('#', '');
 
-    // If the hex number is like 333, double its length so it's a proper 6 character hex number
-    // e.g. 333 => 333333 and 1A1 => 1A11A1
-    if (hexNumber.length === 3) {
-        hexNumber = hexNumber + hexNumber;
-    }
+  // If the hex number is like 333, double its length so it's a proper 6 character hex number
+  // e.g. 333 => 333333 and 1A1 => 1A11A1
+  if (hexNumber.length === 3) {
+    hexNumber += hexNumber;
+  }
 
-    // Parse the hex number into an actual number
-    const hexAsNum = parseInt(hexNumber, 16);
+  // Parse the hex number into an actual number
+  const hexAsNum = parseInt(hexNumber, 16);
 
-    // Convert to RGB
-    let r = (hexAsNum >> 16) & 0xff;
-    let g = (hexAsNum >> 8) & 0xff;
-    let b = hexAsNum & 0xff;
+  // Convert to RGB
+  let r = (hexAsNum >> 16) & 0xff;
+  let g = (hexAsNum >> 8) & 0xff;
+  let b = hexAsNum & 0xff;
 
-    const _clamp = (color: number): number => {
-        return Math.max(0, Math.min(255, Math.round(color * (1 - percent / 100))));
-    }
+  const _clamp = (color: number): number => {
+    return Math.max(0, Math.min(255, Math.round(color * (1 - percent / 100))));
+  };
 
-    // Clamp values
-    r = _clamp(r);
-    g = _clamp(g);
-    b = _clamp(b);
+  // Clamp values
+  r = _clamp(r);
+  g = _clamp(g);
+  b = _clamp(b);
 
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
