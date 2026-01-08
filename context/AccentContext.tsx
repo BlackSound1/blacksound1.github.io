dfa5dcff75c4b3c7f1ea4f92164440506327e1d2
BlackSound1 <matthewr@videotron.ca>
2026-01-07 21:09:25 -0500

    Prettier'd ./context

--- context/AccentContext.tsx
+++ context/AccentContext.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useMemo, useState } from "react";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useMemo, useState } from 'react';
 
-import { darkenHex } from "@/lib/utils";
-
+import { darkenHex } from '@/lib/utils';
 
 type AccentContextType = {
-    accent: string;
-    setAccent: Dispatch<SetStateAction<string>>;
+  accent: string;
+  setAccent: Dispatch<SetStateAction<string>>;
 };
 
 // Create the AccentContext
@@ -19,38 +18,31 @@ const AccentContext = createContext<AccentContextType | undefined>(undefined);
  * @returns The AccentContext provider.
  */
 export const AccentProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [accent, setAccent] = useState<string>('#eb575a');
+  const [accent, setAccent] = useState<string>('#eb575a');
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            const accentColor = localStorage.getItem('accent');
-            if (accentColor) {
-                setAccent(accentColor);
-            }
-        }
-    }, [])
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const accentColor = localStorage.getItem('accent');
+      if (accentColor) {
+        setAccent(accentColor);
+      }
+    }
+  }, []);
 
-    useEffect(() => {
-        if (typeof document === "undefined") {
-            return;
-        }
-        document.documentElement.style.setProperty("--accent", accent);
-        document.documentElement.style.setProperty("--scrollbar-thumb", accent);
-        document.documentElement.style.setProperty("--scrollbar-thumb-hover", darkenHex(accent, 20));
-        document.documentElement.style.setProperty("--sc-color", accent);
-        localStorage.setItem('accent', accent);
-    }, [accent]);
+  useEffect(() => {
+    if (typeof document === 'undefined') {
+      return;
+    }
+    document.documentElement.style.setProperty('--accent', accent);
+    document.documentElement.style.setProperty('--scrollbar-thumb', accent);
+    document.documentElement.style.setProperty('--scrollbar-thumb-hover', darkenHex(accent, 20));
+    document.documentElement.style.setProperty('--sc-color', accent);
+    localStorage.setItem('accent', accent);
+  }, [accent]);
 
-    const value = useMemo(
-        () => ({ accent, setAccent }),
-        [accent, setAccent],
-    );
+  const value = useMemo(() => ({ accent, setAccent }), [accent, setAccent]);
 
-    return (
-        <AccentContext.Provider value={value}>
-            {children}
-        </AccentContext.Provider>
-    );
+  return <AccentContext.Provider value={value}>{children}</AccentContext.Provider>;
 };
 
 /**
@@ -58,9 +50,9 @@ export const AccentProvider: React.FC<{ children: ReactNode }> = ({ children }) 
  * @returns The accent context.
  */
 export function useAccent(): AccentContextType {
-    const ctx = useContext(AccentContext);
-    if (!ctx) {
-        throw new Error("useAccent must be used within an AccentProvider");
-    }
-    return ctx;
-};
+  const ctx = useContext(AccentContext);
+  if (!ctx) {
+    throw new Error('useAccent must be used within an AccentProvider');
+  }
+  return ctx;
+}
