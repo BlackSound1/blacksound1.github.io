212f5e11fec44c246e5c8d86522a07a8fccaddb0
BlackSound1 <matthewr@videotron.ca>
2026-01-07 19:52:05 -0500

    Big-ish refactor to put the Footer inside the main body so it's the correct size

--- app/page.tsx
+++ app/page.tsx
@@ -11,24 +11,20 @@ export default function Home() {
   posthog.capture('$pageview', { page: 'homepage' });
 
   return (
-    <div className="text-text mx-auto flex min-h-screen max-w-[90%] flex-col md:max-w-[80%]">
-      <main className="flex-1 px-0 py-8 md:px-5">
-        <div className="mx-auto max-w-6xl space-y-12 px-0 py-8 md:space-y-16 md:px-4 md:py-12">
-          <AboutMeSection />
+    <div>
+      <AboutMeSection />
 
-          <Break />
+      <Break />
 
-          <WorkSection />
+      <WorkSection />
 
-          <Break />
+      <Break />
 
-          <ProjectSection />
+      <ProjectSection />
 
-          <Break />
+      <Break />
 
-          <FunSection />
-        </div>
-      </main>
+      <FunSection />
     </div>
   );
 }
